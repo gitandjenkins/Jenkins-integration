05406372f4e2627fa4557a98c8c5ee34178bdb34
Naeyl <adamautomationtester@gmail.com>
2023-09-28 23:46:19 -0400

    chromeDriver added

--- src/test/java/first/Project/ClassOne.java
+++ src/test/java/first/Project/ClassOne.java
@@ -1,10 +1,24 @@
 package first.Project;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class ClassOne {
+	
+	public static void main(String[] args){
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver.exe");
 
-	public static void main(String[] args) {
-System.out.println("Good MOrning ");
-
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.walmart.com/");
+		
+		driver.close();
+		
+		
+		
+		
 	}
-
 }
